191c638fd0f340941ac7de3f40d4842b4a89034f
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-03-08 16:45:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 tb_token = os.getenv("TB_TOKEN", "")
 csftp_link = os.getenv("CSFTP_LINK" , "")
+cronchecklink = os.getenv("CRON_CHECK_LINK", "")
 fydevlink = os.getenv("FYDEV_LINK", "")
 fy_code = os.getenv("FY_APP_CODE", "")
 fy_app_staging = os.getenv("FY_APP_STAGING", "")
@@ -10,5 +11,5 @@ fy_app_prod = os.getenv("FY_APP_PROD", "")
 username = os.getenv("FYDEV_USER", "")
 password = os.getenv("FYDEV_PASS", "")
 
-cronchecklink = os.getenv("CRON_CHECK_LINK", "")
+
 afwanemail = os.getenv("AFWAN_EMAIL", "")
