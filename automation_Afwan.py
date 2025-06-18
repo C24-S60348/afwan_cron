f51cfb3e306a3988f4d32c7bd2cfa739af0bf226
Afwan <71675063+C24-S60348@users.noreply.github.com>
2025-06-18 16:15:55 +0800

    sftp

--- automation_Afwan.py
+++ automation_Afwan.py
@@ -864,7 +864,7 @@ def run_function(program_code, code2=None, info3=None):
             #html_data = response
             #print(html_data)
             if (html_data != "connected to NAVITAIRE1SAP<br/>connected to NPS1FIREFLY<br/>connected to ELNVOICE1NAVITAIRE<br/>connected to 2360692 prod<br/>connected to 2360692 staging"): #has changes
-                run_function("TB", "FYSFTP", f"One of the SFTP is not running  \n  \n  {html_data} \n \n {url}")
+                run_function("TB", "FYSFTP", f"One of the SFTP is not running  \n  \n  {html_data} \n \n on payment_feed_extract/cron_test_sftp.php")
             print (html_data)
             print("done CSFTP")
 
