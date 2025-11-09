a5eb7a904fd3a9679bfd04c6b21d5ce242addf7a
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-11-09 08:58:00 +0800

    .

--- flask_page/controllers/home.py
+++ flask_page/controllers/home.py
@@ -8,38 +8,39 @@ home_bp = Blueprint('home_bp', __name__)
 TXT_FILES_DIR = "/home/AfwanProductions/mysite/cron_output/"
 @home_bp.route("/")
 def index():
-    return """
+    html = """
     <html>
         <body>
             <h1>This is Afwan's server</h1>
         </body>
     </html>
     """
+    return render_template_string(html)
     
-    txt_files = [f for f in os.listdir(TXT_FILES_DIR) if f.endswith(".txt")]
-    txt_files.sort(reverse=True)
-    now = datetime.now(timezone.utc)
-    gmt8 = timezone(timedelta(hours=8))
-    current_time = now.astimezone(gmt8).strftime('%Y-%m-%d %I:%M %p')
-    last_run_time = datetime.fromtimestamp(max(last_run_times.values()), tz=gmt8).strftime('%Y-%m-%d %I:%M %p')
-    html = f"""
-        <!DOCTYPE html>
-        <html>
-        <head><title>TXT Files</title></head>
-        <body>
-            <h2>TXT Files from afwanhaziq.pythonanywhere.com</h2>
-            <p>Current time: {current_time}</p>
-            <p>Last run cron: {last_run_time}</p>
-            <ul>"""
+    # txt_files = [f for f in os.listdir(TXT_FILES_DIR) if f.endswith(".txt")]
+    # txt_files.sort(reverse=True)
+    # now = datetime.now(timezone.utc)
+    # gmt8 = timezone(timedelta(hours=8))
+    # current_time = now.astimezone(gmt8).strftime('%Y-%m-%d %I:%M %p')
+    # last_run_time = datetime.fromtimestamp(max(last_run_times.values()), tz=gmt8).strftime('%Y-%m-%d %I:%M %p')
+    # html = f"""
+    #     <!DOCTYPE html>
+    #     <html>
+    #     <head><title>TXT Files</title></head>
+    #     <body>
+    #         <h2>TXT Files from afwanhaziq.pythonanywhere.com</h2>
+    #         <p>Current time: {current_time}</p>
+    #         <p>Last run cron: {last_run_time}</p>
+    #         <ul>"""
     
-    for file in txt_files:
-        html += f"<li><a href='/view/{file}'>{file}</a></li>"
-    html += """
-            </ul>
-        </body>
-        </html>
-    """
-    return render_template_string(html, files=txt_files)
+    # for file in txt_files:
+    #     html += f"<li><a href='/view/{file}'>{file}</a></li>"
+    # html += """
+    #         </ul>
+    #     </body>
+    #     </html>
+    # """
+    # return render_template_string(html, files=txt_files)
 
 
 @home_bp.route("/view/<filename>")
