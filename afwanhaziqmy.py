57076de4e442efbc1f25321d8de31fd4667041f0
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-11-16 02:57:19 +0800

    .

--- afwanhaziqmy.py
+++ afwanhaziqmy.py
@@ -18,6 +18,7 @@ import secrets
 import httpx
 import variables
 import traceback
+from flask_page.utils.outsource_helper import *
 
 
 app = Flask(
@@ -80,7 +81,8 @@ def handle_exception(e):
             str(data)
         ]
         #log here
-        send_telegram_error(errormessage)
+        errormessage = f"🚨 ERROR ALERT 🚨\n\n{errormessage}"
+        modelsendtelegrammessage(errormessage)
         af_addcsv("static/db/error/error.csv", new_data)
     
     return jsonify({
@@ -90,25 +92,7 @@ def handle_exception(e):
     })
 
 
-def send_telegram_error(error_message):
-    try:
-        bot_token = variables.tb_token_server
-        chat_id = "-1002864663247"
 
-        url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-        data = {
-            "chat_id": chat_id,
-            "text": f"🚨 ERROR ALERT 🚨\n\n{error_message}",
-            "parse_mode": "HTML"
-        }
-
-        with httpx.Client() as client:
-            response = client.post(url, data=data)
-
-        if response.status_code != 200:
-            print(f"Failed to send Telegram message: {response.text}")
-    except Exception as e:
-        print(f"Error sending Telegram message: {e}")
 
 from flask_page.controllers.apitest import apitest_bp
 from flask_page.controllers.connect_to_db import connect_to_db
