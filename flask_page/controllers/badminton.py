8136f630ae4c1d3245716a4b8d991285883ca6bd
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2026-01-06 23:30:14 +0800

    .

--- flask_page/controllers/badminton.py
+++ flask_page/controllers/badminton.py
@@ -1,8 +1,8 @@
 #controllers/badminton.py
 import random
 from flask import abort, jsonify, request, Blueprint, render_template, render_template_string, send_from_directory, session
-from ..utils.html_helper import *
-from ..utils.db_helper import *
+from apps.utils.html_helper import *
+from apps.utils.db_helper import *
 from datetime import datetime
 import sqlite3
 import random
@@ -39,9 +39,48 @@ def init_db():
             created_at TEXT
         )
     ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS history (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            date TEXT,
+            time TEXT,
+            score TEXT DEFAULT "0-0",
+            remark TEXT,
+            playerleft TEXT,
+            playerright TEXT,
+            userid TEXT,
+            created_at TEXT
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS history (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            date TEXT,
+            time TEXT,
+            score TEXT DEFAULT "0-0",
+            remark TEXT,
+            playerleft TEXT,
+            playerright TEXT,
+            userid TEXT,
+            created_at TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS suggestions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            comment TEXT,
+            created_at TEXT
+        )
+    ''')
 
     # Insert default
     cursor.execute("INSERT OR IGNORE INTO users (name) VALUES (?)", ('this is sample',))
+    cursor.execute("INSERT OR IGNORE INTO history (name) VALUES (?)", ('this is sample',))
+    cursor.execute("INSERT OR IGNORE INTO suggestions (name) VALUES (?)", ('this is sample',))
 
     db.commit()
     db.close()
@@ -97,3 +136,97 @@ def query():
     dbdata = af_getdb(DB_FILE, query, params)
     return jsonify(dbdata)
 #---------------------------------------------
+
+@badminton_bp.route('/badminton/api/gethistory', methods=['GET', 'POST'])
+def gethistory():
+
+    # dbloc = getpostget("dbloc")
+    userid = getpostget("userid")
+    query = "SELECT * FROM history WHERE userid = ?"
+    params = (userid,)
+    
+    dbdata = af_getdb(DB_FILE, query, params)
+    return jsonify(dbdata)
+
+
+@badminton_bp.route('/badminton/api/inserthistory', methods=['GET', 'POST'])
+def inserthistory():
+
+    query = "INSERT INTO history (userid,date,playerleft,playerright,remark,score,time,created_at)"
+    query += " VALUES (?,?,?,?,?,?,?,?)"
+    
+    userid = getpostget("userid")
+    date = getpostget("date")
+    playerleft = getpostget("playerleft")
+    playerright = getpostget("playerright")
+    remark = getpostget("remark")
+    score = getpostget("score")
+    time = getpostget("time")
+    created_at = datetime.now()
+
+    params = (userid,date,playerleft,playerright,remark,score,time,created_at,)
+    
+    dbdata = af_getdb(DB_FILE, query, params)
+    return jsonify(dbdata)
+
+@badminton_bp.route('/badminton/api/updatehistory', methods=['GET', 'POST'])
+def updatehistory():
+
+    query = "UPDATE history"
+    query += " SET userid = ?,date = ?,playerleft = ?,playerright = ?,remark = ?,score = ?,time = ?,created_at = ?)"
+    query += " WHERE ID = ?"
+    
+    userid = getpostget("userid")
+    date = getpostget("date")
+    playerleft = getpostget("playerleft")
+    playerright = getpostget("playerright")
+    remark = getpostget("remark")
+    score = getpostget("score")
+    time = getpostget("time")
+    created_at = datetime.now()
+    id = getpostget("id")
+
+    params = (userid,date,playerleft,playerright,remark,score,time,created_at,id,)
+    
+    dbdata = af_getdb(DB_FILE, query, params)
+    return jsonify(dbdata)
+
+def getsummary():
+    userid = getpostget("userid")
+    query = "SELECT * FROM history WHERE userid = ?"
+    params = (userid,)
+    
+    dbdata = af_getdb(DB_FILE, query, params)
+
+    name = ""
+    count = 0
+    result = "Summary: "
+
+    #count the player how many wins
+    for db in dbdata:
+        name = db['playerleft']
+        count = 0
+        for db2 in dbdata:
+            if db2['playerleft'] == name:
+                if db2['score'] == "left": #left side of "-" is winner - means the name is winner
+                    count += 1
+            if db2['playerright'] == name:
+                if db2['score'] == "right": #right side of "-" is winner - means the name is winner
+                    count += 1
+        result += f"{name} : {count}, "
+            
+    return jsonify(result)
+
+def insertsuggestions():
+
+    query = "INSERT INTO suggestions (name,comment,created_at)"
+    query += " VALUES (?,?,?)"
+    
+    name = getpostget("name")
+    comment = getpostget("comment")
+    created_at = datetime.now()
+
+    params = (name,comment,created_at,)
+    
+    dbdata = af_getdb(DB_FILE, query, params)
+    return jsonify(dbdata)
